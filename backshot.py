0fda93219d9585fdf7a5e3257e678974f058aaa3
redstone59 <109836856+redstone59@users.noreply.github.com>
2024-02-06 23:17:59 +1000

    add to-be-implemented functions
    comments explain how they should work

--- backshot.py
+++ backshot.py
@@ -1,1 +1,15 @@
 from roulette import *
+
+class BackshotRoulette:
+    def __init__(self):
+        pass
+    
+    def evaluate(self, state: BuckshotRouletteMove):
+        # Evaluate how good the current players position is.
+        # Should utilise turn probability, health, item count, knowing shells, etc.
+        pass
+    
+    def search(self):
+        # Should return the best move for any given position. Use recursion and alpha-beta pruning and whatever.
+        # Basically, create a tree of every possible move that can be done, evaluate, and select the best move.
+        pass
