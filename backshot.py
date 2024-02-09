12fce1e6fefa0e78a790c2eab613d22539cd2818
redstone59 <109836856+redstone59@users.noreply.github.com>
2024-02-09 16:26:10 +1000

    pull stuff out to functions, add new bonuses
    added item bonus to make the bot conserve valuable items
    added hand saw bonus to reward having more than 2 health if there is a hand saw in the other players inventory
    known shell bonus pulled to own function

--- backshot.py
+++ backshot.py
@@ -2,11 +2,14 @@ from roulette import *
 
 INF = mpf(inf)
 
-def max_or_min(is_players_turn: bool, a, b):
-    if a == None: return b
-    if b == None: return a
-    if is_players_turn: return max(a, b)
-    return min(a, b)
+def hand_saw_bonus(state: BuckshotRouletteMove):
+    if state.is_players_turn and Items.HAND_SAW in state.dealer_items and state.player_health > 2:
+        return 150
+    
+    elif (not state.is_players_turn) and Items.HAND_SAW in state.player_items and state.dealer_health > 2:
+        return 150
+    
+    return 0
 
 def is_redundant_move(move: ValidMoves, state: BuckshotRouletteMove):
     """
@@ -42,8 +45,56 @@ def is_redundant_move(move: ValidMoves, state: BuckshotRouletteMove):
         
         case ValidMoves.USE_MAGNIFYING_GLASS:
             if state.current_shell != None: return True
+            if state.live_shells == 0 or state.blank_shells == 0: return True
     
     return False
+
+def item_bonus(items: list):
+    bonus = 0
+    
+    for item in items:
+        match item:
+            case Items.BEER:
+                bonus += 10
+            case Items.CIGARETTES:
+                bonus += 30
+            case Items.HANDCUFFS:
+                bonus += 50
+            case Items.HAND_SAW:
+                bonus += 60
+            case Items.MAGNIFYING_GLASS:
+                bonus += 90
+    
+    return bonus
+
+def known_shell_bonus(move: ValidMoves, state: BuckshotRouletteMove):
+    bonus = 0
+    
+    if state.current_shell == "live":
+        bonus += 200 * (1 if state.is_players_turn else -1)
+        if state.is_players_turn and move != ValidMoves.SHOOT_DEALER: return -INF
+        elif (not state.is_players_turn) and move != ValidMoves.SHOOT_PLAYER: return INF
+    
+    elif state.current_shell == "blank":
+        bonus += 100 * (1 if state.is_players_turn else -1)
+        if state.is_players_turn and move != ValidMoves.SHOOT_PLAYER: return -INF
+        elif (not state.is_players_turn) and move != ValidMoves.SHOOT_DEALER: return INF
+    
+    elif state.current_shell == None:
+        chance_live_loaded = state.live_shells / (state.live_shells + state.blank_shells)
+        
+        if state.is_players_turn and move != ValidMoves.SHOOT_DEALER: chance_live_loaded *= -1
+        elif (not state.is_players_turn) and move != ValidMoves.SHOOT_PLAYER: chance_live_loaded *= -1
+        
+        bonus += chance_live_loaded * 100
+    
+    return bonus
+
+def max_or_min(is_players_turn: bool, a, b):
+    if a == None: return b
+    if b == None: return a
+    if is_players_turn: return max(a, b)
+    return min(a, b)
 
 class Move:
     def __init__(self, move_type, evaluation):
@@ -69,23 +120,11 @@ class BackshotRoulette:
         if state.player_health == 0: state_eval =  -INF
         if state.dealer_health == 0: state_eval = INF
         
-        if state.current_shell == "live": 
-            state_eval += 100 * (1 if state.is_players_turn else -1)
-            if state.is_players_turn and move != ValidMoves.SHOOT_DEALER: return -INF
-            elif (not state.is_players_turn) and move != ValidMoves.SHOOT_PLAYER: return INF
+        state_eval += known_shell_bonus(move, state)
+        state_eval += hand_saw_bonus(state)
         
-        elif state.current_shell == "blank":
-            state_eval += 100 * (1 if state.is_players_turn else -1)
-            if state.is_players_turn and move != ValidMoves.SHOOT_PLAYER: return -INF
-            elif (not state.is_players_turn) and move != ValidMoves.SHOOT_DEALER: return INF
-        
-        elif state.current_shell == None:
-            chance_live_loaded = state.live_shells / (state.live_shells + state.blank_shells)
-            
-            if state.is_players_turn and move != ValidMoves.SHOOT_DEALER: chance_live_loaded *= -1
-            elif (not state.is_players_turn) and move != ValidMoves.SHOOT_PLAYER: chance_live_loaded *= -1
-            
-            state_eval += chance_live_loaded * 50
+        state_eval += item_bonus(state.player_items)
+        state_eval -= item_bonus(state.dealer_items)
         
         state_eval += state.player_health * 25
         state_eval -= state.dealer_health * 25
@@ -109,11 +148,7 @@ class BackshotRoulette:
         best_move = Move(None, INF * -1 if state.is_players_turn else 1)
 
         for move in all_moves:
-            try:
-                next_state = state.move(move)
-            except InvalidMoveError:
-                print("invalid move", move)
-                continue
+            next_state = state.move(move)
             
             if is_redundant_move(move, state) or next_state == None: continue
 
